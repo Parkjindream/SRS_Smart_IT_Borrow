952cd536698f9567f5d18f50a267d021393395b8
Parkjindream <fhfg09820@gmail.com>
2026-09-25 23:58:07 +0700

    ยังแก้ไขงานอยู่

--- inventory/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Equipment
-
-@admin.register(Equipment)
-class EquipmentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'serial_number', 'status', 'available_quantity', 'total_quantity')
-    list_filter = ('category', 'status')
-    search_fields = ('name', 'serial_number', 'category')
